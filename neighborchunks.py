7fa982d3486218d8b20dd2606d657554aedaca33
tagoma <23656943+tagomatech@users.noreply.github.com>
2023-05-31 00:33:52 +0200

    Update neighborchunks.py

--- neighborchunks.py
+++ neighborchunks.py
@@ -3,18 +3,27 @@
 
 from blp import blp
 from typing import Union
+from sklearn.neighbors import KDTree
 import datetime as dt
 import numpy as np
 import pandas as pd
 
 
+
 class NeighborChunks:
-    
-    def __init__(self, df:pd.DataFrame, nb_of_chunks:Union[int, None]=None, chunk_length:int=10) -> pd.DataFrame:
-   
+    def __init__(self,
+                 df:pd.DataFrame,
+                 nb_of_chunks:Union[int, None]=None,
+                 chunk_length:int=5,
+                 nb_neighors:int=5,
+                 leaf_size=40,
+                 metric='minkowski'):
+
         self.df = df
         self.ser = df['Return']
         self.ser.index = df['Date']
+        
+        # Create chunks
         ser_in = self.ser.dropna()
         ser_len = ser_in.shape[0]
 
@@ -42,12 +51,15 @@ class NeighborChunks:
         f = chunk_length
         d = 0
 
+        #print(ser_in)
         for i in list(range(ser_in.shape[0], 0, -1)):
             lst_chunk.append(c)
             lst_feat.append(f)
             lst_dat.append(ser_in.index[i - 1 + d])
             lst_val.append(ser_in.values[i - 1 + d])
+
             f -= 1
+
             if f == 0:
                 c -= 1
                 f = chunk_length
@@ -59,14 +71,48 @@ class NeighborChunks:
                               'Feature' : lst_feat,
                              })
         
-        df_final = pd.merge(df_in, df, on='Date', how='inner')
-        df_final = df_final.drop(columns=['Return_x'])
-        df_final = df_final.rename(columns={'Return_y' : 'Return'})
-        df_final = df_final.sort_values(by=['Chunk', 'Feature'])
+        df_chunks = pd.merge(df_in, df, on='Date', how='inner')
+        df_chunks = df_chunks.drop(columns=['Return_x'])
+        df_chunks = df_chunks.rename(columns={'Return_y' : 'Return'})
+        df_chunks = df_chunks.sort_values(by=['Chunk', 'Feature'])
         
-        self.df_final = df_final.reset_index(drop=True)
+        self.df_chunks = df_chunks.reset_index(drop=True)
+        
+
+        # Compute neighbor
+        X = df_chunks[df_chunks['Chunk'] < nb_of_chunks].pivot(columns=['Feature'], index='Chunk',  values='Return').values
+        y = df_chunks[df_chunks['Chunk'] == nb_of_chunks].pivot(columns=['Feature'], index='Chunk',  values='Return').values
+
+        kdt = KDTree(X, leaf_size=leaf_size, metric=metric)
+
+        def get_indices(idx, chunk_length):
+            idx_min = chunk_length*idx#+chunk_length-1
+            return (idx_min, idx_min+chunk_length-1)
+
+        dist, idx  = kdt.query(y, k=nb_neighors, return_distance=True)
+
+        idx = [y for x in idx for y in x]
+        #idx = [y for x in idx for y in x]
+        print(idx)
+        if len(idx) == 1:
+            idx = idx[0]
+            indices = indices(idx, chunk_length)
+        else:
+            indices = []
+            for i in idx:
+                indices.append(get_indices(i, chunk_length))
+
+        df_chunks = df_chunks.assign(Neighbor = np.nan)
+
+        n = 1
+        for i in indices:
+            df_chunks.loc[(df_chunks.index >= i[0]) & (df_chunks.index <= i[1]), 'Neighbor'] = str(n)
+            n += 1
+
+        df_chunks.loc[df_chunks['Neighbor'].isna(), 'Neighbor'] = 'target'
+
+        self.df_chunks = df_chunks#print(indices(idx, chunk_length))
 
     
     def get(self):
-        
-        return self.df_final
+        return self.df_chunks  
